a81118171369ddf9f12bcdbf6288aadfd91b69e7
panzz09 <irvanirvan@gmail.com>
2023-09-29 21:17:07 +0700

    add createServer

--- src/Infrastructures/http/_test/createServer.test.js
+++ src/Infrastructures/http/_test/createServer.test.js
@@ -15,23 +15,23 @@ describe('HTTP server', () => {
     expect(response.statusCode).toEqual(404);
   });
 
-  // describe('when GET /', () => {
-  //   it('should return 200 and hello world', async () => {
-  //     // Arrange
-  //     const server = await createServer({});
+  describe('when GET /', () => {
+    it('should return 200 and hello world', async () => {
+      // Arrange
+      const server = await createServer({});
 
-  //     // Action
-  //     const response = await server.inject({
-  //       method: 'GET',
-  //       url: '/',
-  //     });
+      // Action
+      const response = await server.inject({
+        method: 'GET',
+        url: '/',
+      });
 
-  //     // Assert
-  //     const responseJson = JSON.parse(response.payload);
-  //     expect(response.statusCode).toEqual(200);
-  //     expect(responseJson.value).toEqual('Hello world!');
-  //   });
-  // });
+      // Assert
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(200);
+      expect(responseJson.value).toEqual('Hello world!');
+    });
+  });
 
   it('should handle server error correctly', async () => {
     // Arrange
